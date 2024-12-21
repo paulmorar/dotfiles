d83b176874682309789ac7d8dacf5488794d8b9c
paulmorar <paulcornelmorar@gmail.com>
2024-12-21 16:27:49 +0100

    Remove all files

--- .config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
--- Docs: https://github.com/catppuccin/nvim
-return { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end
-}
